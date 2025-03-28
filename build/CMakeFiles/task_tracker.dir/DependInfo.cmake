3d35748263f332606cf4b25b88d493dfd5eb1f0b
yoga <508711043@qq.com>
2025-03-28 19:48:34 +0800

    task-tracker

---
+++ build/CMakeFiles/task_tracker.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/klapat/cpp/yogaMac/ITERM/taskTracker/otherVertion/internal/task/task.cc" "CMakeFiles/task_tracker.dir/internal/task/task.cc.o" "gcc" "CMakeFiles/task_tracker.dir/internal/task/task.cc.o.d"
+  "/home/klapat/cpp/yogaMac/ITERM/taskTracker/otherVertion/main.cc" "CMakeFiles/task_tracker.dir/main.cc.o" "gcc" "CMakeFiles/task_tracker.dir/main.cc.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/task_tracker.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/task_tracker.dir/internal/task/task.cc.o"
+  "CMakeFiles/task_tracker.dir/internal/task/task.cc.o.d"
+  "CMakeFiles/task_tracker.dir/main.cc.o"
+  "CMakeFiles/task_tracker.dir/main.cc.o.d"
+  "task_tracker"
+  "task_tracker.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/task_tracker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
